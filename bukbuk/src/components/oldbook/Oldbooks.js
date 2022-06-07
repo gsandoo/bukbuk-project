3bcfd19db7161348968435254acaf4a61c2e5af1
gsandoo <rogersddt@naver.com>
2022-06-07 21:16:48 +0900

    지도

--- bukbuk/src/components/oldbook/Oldbooks.js
+++ bukbuk/src/components/oldbook/Oldbooks.js
@@ -1,20 +1,28 @@
-import React,{useRef, useEffect} from "react";
+import React,{useEffect} from "react";
 import Menu from "../navibar/Menu";
 import Nav from "../navibar/Nav";
 import '../../css files/map.css';
-import Map from "./Map";
+
 
 
 
 function Oldbooks(){
+    const {kakao} = window;
+    useEffect(() => {
+        const container = document.getElementById('myMap');
+		const options = {
+			center: new kakao.maps.LatLng(33.450701, 126.570667),
+			level: 3
+		};
+        const map = new kakao.maps.Map(container, options);
+    }, []);
    return(
         <div>
             <Nav/>
             <Menu/>
             <div id="map">
-                <div className="map-container">
-                <Map/>
-                </div>
+                
+                
             </div>
         </div>
     );
--- bukbuk/src/components/oldbook/oldbookapi.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const KEY ='8f6f1058-471c-4819-a9d2-669af4859a92';
-
-
-const Oldbook=  axios.get('http://api.kcisa.kr/API_CNV_045/request?serviceKey=`${KEY}`')
-.then(data=>{console.log(data)})
-
-export default Oldbook;
