7b9846399987221fb62f9cf58e4d3a8ccdbde76a
gsandoo <rogersddt@naver.com>
2022-06-09 17:01:08 +0900

    ckeditor

--- bukbuk/src/components/service/ServiceWrite.js
+++ bukbuk/src/components/service/ServiceWrite.js
@@ -11,8 +11,26 @@ function ServiceWrite(){
             <main className="service-main">
                 <div id="writedown">
                     <input className="title-input" type="text" placeholder="제목을 입력해주세요"/>
-                    <textarea className="text-area" placeholder="내용을 입력해주세요"></textarea>
-                    <button>입력</button>
+                    {/* <textarea className="text-area" placeholder="내용을 입력해주세요"></textarea> */}
+                    <CKEditor
+                        editor={ClassicEditor}
+                        data="<p>Hello from CKEditor 5!</p>"
+                        onReady={editor => {
+                            // You can store the "editor" and use when it is needed.
+                            console.log('Editor is ready to use!', editor);
+                        }}
+                        onChange={(event, editor) => {
+                            const data = editor.getData();
+                            console.log({ event, editor, data });
+                        }}
+                        onBlur={(event, editor) => {
+                            console.log('Blur.', editor);
+                        }}
+                        onFocus={(event, editor) => {
+                            console.log('Focus.', editor);
+                        }}
+                    />
+                    <button className="submit">입력</button>
                     <Link to={'/Service'}><button className="go-back">돌아가기</button></Link>
                 </div>
             </main>
